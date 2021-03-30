f94a8c250f0bcc75185d4bbcaf62cfb341320060
oskarafroo <54503313+oskarafroo@users.noreply.github.com>
2021-03-30 22:08:36 +0200

    added app title, task field and task description field

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,82 @@ namespace Timesheet
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAppName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelAppName
+            // 
+            this.labelAppName.AutoSize = true;
+            this.labelAppName.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelAppName.Location = new System.Drawing.Point(346, 41);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(87, 15);
+            this.labelAppName.TabIndex = 0;
+            this.labelAppName.Text = "TimeSheet App";
+            this.labelAppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelAppName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelTask
+            // 
+            this.labelTask.AutoSize = true;
+            this.labelTask.Location = new System.Drawing.Point(39, 107);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(29, 15);
+            this.labelTask.TabIndex = 2;
+            this.labelTask.Text = "Task";
+            this.labelTask.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(39, 147);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(67, 15);
+            this.labelDescription.TabIndex = 3;
+            this.labelDescription.Text = "Description";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(764, 475);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelTask);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelAppName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAppName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelTask;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,14 @@ namespace Timesheet
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
