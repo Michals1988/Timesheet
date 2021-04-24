417f231b2ce48ff6efa88a8234cb26c4203f6423
Michals1988 <michal.seifert@wp.pl>
2021-04-25 00:21:16 +0200

    -add simple validation -add firestore (database) -add login and password textbox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Timesheet
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
             this.labelAppName = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelTask = new System.Windows.Forms.Label();
@@ -37,22 +38,39 @@ namespace Timesheet
             this.labelTimeFrom = new System.Windows.Forms.Label();
             this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
             this.labelTimeTo = new System.Windows.Forms.Label();
-            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
             this.labelTotal = new System.Windows.Forms.Label();
             this.labelTotalHours = new System.Windows.Forms.Label();
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.labelDate = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.CustomFormat = "dd-MM-yyyy   HH:mm";
+            this.dateTimePickerTo.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerTo.Location = new System.Drawing.Point(467, 305);
+            this.dateTimePickerTo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.ShowUpDown = true;
+            this.dateTimePickerTo.Size = new System.Drawing.Size(228, 27);
+            this.dateTimePickerTo.TabIndex = 8;
+            this.dateTimePickerTo.ValueChanged += new System.EventHandler(this.dateTimePickerTo_ValueChanged);
             // 
             // labelAppName
             // 
             this.labelAppName.AutoSize = true;
-            this.labelAppName.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.labelAppName.Location = new System.Drawing.Point(307, 37);
+            this.labelAppName.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelAppName.Font = new System.Drawing.Font("ISOCPEUR", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelAppName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelAppName.Location = new System.Drawing.Point(262, 9);
             this.labelAppName.Name = "labelAppName";
-            this.labelAppName.Size = new System.Drawing.Size(87, 15);
+            this.labelAppName.Size = new System.Drawing.Size(230, 41);
             this.labelAppName.TabIndex = 0;
             this.labelAppName.Text = "TimeSheet App";
             this.labelAppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -60,42 +78,44 @@ namespace Timesheet
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(114, 104);
+            this.textBox1.Location = new System.Drawing.Point(130, 139);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(495, 23);
+            this.textBox1.Size = new System.Drawing.Size(565, 27);
             this.textBox1.TabIndex = 1;
             // 
             // labelTask
             // 
             this.labelTask.AutoSize = true;
-            this.labelTask.Location = new System.Drawing.Point(39, 107);
+            this.labelTask.Location = new System.Drawing.Point(45, 143);
             this.labelTask.Name = "labelTask";
-            this.labelTask.Size = new System.Drawing.Size(32, 15);
+            this.labelTask.Size = new System.Drawing.Size(39, 20);
             this.labelTask.TabIndex = 2;
             this.labelTask.Text = "Task:";
             // 
             // labelDescription
             // 
             this.labelDescription.AutoSize = true;
-            this.labelDescription.Location = new System.Drawing.Point(39, 147);
+            this.labelDescription.Location = new System.Drawing.Point(45, 196);
             this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(70, 15);
+            this.labelDescription.Size = new System.Drawing.Size(88, 20);
             this.labelDescription.TabIndex = 3;
             this.labelDescription.Text = "Description:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 144);
+            this.textBox2.Location = new System.Drawing.Point(130, 192);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(495, 23);
+            this.textBox2.Size = new System.Drawing.Size(565, 27);
             this.textBox2.TabIndex = 4;
             // 
             // labelTimeFrom
             // 
             this.labelTimeFrom.AutoSize = true;
-            this.labelTimeFrom.Location = new System.Drawing.Point(39, 235);
+            this.labelTimeFrom.Location = new System.Drawing.Point(45, 313);
             this.labelTimeFrom.Name = "labelTimeFrom";
-            this.labelTimeFrom.Size = new System.Drawing.Size(65, 15);
+            this.labelTimeFrom.Size = new System.Drawing.Size(81, 20);
             this.labelTimeFrom.TabIndex = 5;
             this.labelTimeFrom.Text = "Time from:";
             // 
@@ -103,54 +123,47 @@ namespace Timesheet
             // 
             this.dateTimePickerFrom.CustomFormat = "dd-MM-yyyy   HH:mm";
             this.dateTimePickerFrom.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePickerFrom.Location = new System.Drawing.Point(114, 229);
+            this.dateTimePickerFrom.Location = new System.Drawing.Point(130, 305);
+            this.dateTimePickerFrom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePickerFrom.Name = "dateTimePickerFrom";
-            this.dateTimePickerFrom.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePickerFrom.ShowUpDown = true;
+            this.dateTimePickerFrom.Size = new System.Drawing.Size(228, 27);
             this.dateTimePickerFrom.TabIndex = 6;
             this.dateTimePickerFrom.ValueChanged += new System.EventHandler(this.dateTimePickerFrom_ValueChanged);
             // 
             // labelTimeTo
             // 
             this.labelTimeTo.AutoSize = true;
-            this.labelTimeTo.Location = new System.Drawing.Point(346, 235);
+            this.labelTimeTo.Location = new System.Drawing.Point(395, 313);
             this.labelTimeTo.Name = "labelTimeTo";
-            this.labelTimeTo.Size = new System.Drawing.Size(50, 15);
+            this.labelTimeTo.Size = new System.Drawing.Size(63, 20);
             this.labelTimeTo.TabIndex = 7;
             this.labelTimeTo.Text = "Time to:";
-            // 
-            // dateTimePickerTo
-            // 
-            this.dateTimePickerTo.CustomFormat = "dd-MM-yyyy   HH:mm";
-            this.dateTimePickerTo.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePickerTo.Location = new System.Drawing.Point(409, 229);
-            this.dateTimePickerTo.Name = "dateTimePickerTo";
-            this.dateTimePickerTo.Size = new System.Drawing.Size(200, 23);
-            this.dateTimePickerTo.TabIndex = 8;
-            this.dateTimePickerTo.ValueChanged += new System.EventHandler(this.dateTimePickerTo_ValueChanged);
             // 
             // labelTotal
             // 
             this.labelTotal.AutoSize = true;
-            this.labelTotal.Location = new System.Drawing.Point(39, 269);
+            this.labelTotal.Location = new System.Drawing.Point(45, 359);
             this.labelTotal.Name = "labelTotal";
-            this.labelTotal.Size = new System.Drawing.Size(35, 15);
+            this.labelTotal.Size = new System.Drawing.Size(45, 20);
             this.labelTotal.TabIndex = 9;
             this.labelTotal.Text = "Total:";
             // 
             // labelTotalHours
             // 
             this.labelTotalHours.AutoSize = true;
-            this.labelTotalHours.Location = new System.Drawing.Point(114, 269);
+            this.labelTotalHours.Location = new System.Drawing.Point(130, 359);
             this.labelTotalHours.Name = "labelTotalHours";
-            this.labelTotalHours.Size = new System.Drawing.Size(38, 15);
+            this.labelTotalHours.Size = new System.Drawing.Size(50, 20);
             this.labelTotalHours.TabIndex = 10;
             this.labelTotalHours.Text = "label2";
             // 
             // buttonSubmit
             // 
-            this.buttonSubmit.Location = new System.Drawing.Point(114, 314);
+            this.buttonSubmit.Location = new System.Drawing.Point(130, 419);
+            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonSubmit.Name = "buttonSubmit";
-            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.Size = new System.Drawing.Size(86, 31);
             this.buttonSubmit.TabIndex = 11;
             this.buttonSubmit.Text = "Submit";
             this.buttonSubmit.UseVisualStyleBackColor = true;
@@ -158,9 +171,10 @@ namespace Timesheet
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(409, 314);
+            this.buttonCancel.Location = new System.Drawing.Point(467, 419);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(86, 31);
             this.buttonCancel.TabIndex = 12;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -168,25 +182,64 @@ namespace Timesheet
             // labelDate
             // 
             this.labelDate.AutoSize = true;
-            this.labelDate.Location = new System.Drawing.Point(39, 189);
+            this.labelDate.Location = new System.Drawing.Point(45, 252);
             this.labelDate.Name = "labelDate";
-            this.labelDate.Size = new System.Drawing.Size(34, 15);
+            this.labelDate.Size = new System.Drawing.Size(44, 20);
             this.labelDate.TabIndex = 13;
             this.labelDate.Text = "Date:";
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(114, 183);
+            this.dateTimePicker1.Location = new System.Drawing.Point(130, 244);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.Size = new System.Drawing.Size(228, 27);
             this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 27);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(524, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(171, 27);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "LOGIN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(428, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "PASSWORD:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 413);
+            this.ClientSize = new System.Drawing.Size(757, 551);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.labelDate);
             this.Controls.Add(this.buttonCancel);
@@ -202,8 +255,10 @@ namespace Timesheet
             this.Controls.Add(this.labelTask);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelAppName);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -226,6 +281,10 @@ namespace Timesheet
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Label labelDate;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Cloud.Firestore;
 
 namespace Timesheet
 {
     public partial class Form1 : Form
     {
+        FirestoreDb database;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"cloudfire.json";
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            //database = FirestoreDb.Create("timesheet-3225e");
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -39,22 +50,56 @@ namespace Timesheet
 
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
+
             DateTime dtFrom = dateTimePickerFrom.Value;
             DateTime dtTo = dateTimePickerTo.Value;
+            if (totalHoursCount(dtFrom, dtTo).Equals("ERROR-błędnie podany czas- ujemny czas pracy") ^ (textBox1.Text.Equals("")) ^ (textBox2.Text.Equals("")))
+            {
+                MessageBox.Show("Check the form - not sumbitted");
+            }
 
-                     
-            MessageBox.Show(totalHoursCount(dtFrom, dtTo) + " hours sumbitted!");
+            else
+            {
+                MessageBox.Show(totalHoursCount(dtFrom, dtTo) + " hours sumbitted!");
+            }
 
 
 
         }
 
-        
+
         public string totalHoursCount(DateTime dt1, DateTime dt2)
         {
-            TimeSpan tspan = dt2 - dt1;
-            String timeDiff = tspan.Hours.ToString();
-            return timeDiff;
+            if (dt2 < dt1)
+            {
+                return "ERROR-błędnie podany czas- ujemny czas pracy";
+            }
+            else
+            {
+                TimeSpan tspan = dt2 - dt1;
+                String timeDiff = tspan.Hours.ToString() + ":" + tspan.Minutes.ToString() + ":" + tspan.Seconds.ToString();
+                return timeDiff;
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePickerFrom.Value = dateTimePicker1.Value;
+            dateTimePickerTo.Value = dateTimePicker1.Value;
+        }
+
+
+        void Add_Document() {
+            CollectionReference coll = database.Collection("");
+            Dictionary<string, object> data = new Dictionary<string, object>()
+            {
+                {"Description", textBox2.Text },
+                {"Task", textBox1.Text },
+                {"StartWork", dateTimePickerFrom},
+                {"EndWork", dateTimePickerTo }
+            };
+
+            coll.AddAsync(data);
         }
     }
 }
